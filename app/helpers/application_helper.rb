6af86ca7660c49befd742d624570c22ef009f796
nguyenvanson332000 <nguyenvanson332000@gmail.com>
2022-03-23 14:31:24 +0700

    Chap_4 rails-flavored-ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# module ApplicationHelper
+#   def full_title(page_title='')
+#     base_title = t".title"
+#     if page_title.empty?
+#       base_title
+#     else
+#       page_title + "|" + base_title
+#     end
+#   end
+# end
 module ApplicationHelper
+  def full_title page_title=""
+    base_title = t".title"
+    page_title.empty? ? base_title : page_title + "|" + base_title
+  end
 end
